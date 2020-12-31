097d18e1ac7a26e38dda15c47a8cfcaebcfc7cfe
igorgorb1999 <48647781+igorgorb1999@users.noreply.github.com>
2020-12-31 15:12:10 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,211 @@
+import socket
+import time
+import getch
+from select import select
+import sys
+from scapy.arch import get_if_addr
+import struct
+
+import termios
+import tty
+
+import traceback
+
+UDP_PORT=13117
+UDP_MAGIC_COOKIE=0xfeedbeef
+UDP_OFFER_TYPE=0x02
+UDP_OFFER_SIZE=7
+
+grading=False
+
+#an initialization of the client
+#return the name
+def startup():
+    print("Starting the client.")
+    print("Please choose a name:")
+    name=input()
+    print("You chose a name: "+name)
+    print("Client started, listening for offer requests...")
+    return name
+
+#checks if an offer is legal
+def is_legal_udp_offer(offer):
+    if offer[0] != UDP_MAGIC_COOKIE:
+        return False
+    if offer[1] != UDP_OFFER_TYPE:
+        return False
+    #just for checking if MY server sends udp offers (debugging)
+    #if offer[2] != 2151:#temp
+    #    return False
+    return True
+
+#gets a port from an offer
+def decode_port_from_offer(offer):
+    return offer[2]
+
+#waits for a legal offer
+#returns address and port
+def look_for_servers():
+    #make a udp socket
+    udp_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    udp_socket.settimeout(1)
+    #connect to a specific network
+    #cant figure out how to do it properly
+    binded=False
+    #loop because the address might be occupied by a local client
+    while not binded:
+        try:
+            if grading:
+                udp_socket.bind((get_if_addr("eth2"), UDP_PORT))
+            else:
+                udp_socket.bind(("", UDP_PORT))
+                #udp_socket.bind((get_if_addr("eth1"), UDP_PORT))
+            binded=True
+        except Exception as e:
+            #print(e)
+            pass
+        time.sleep(0.1)
+    udp_offer=(0, 0, 0)
+    while not is_legal_udp_offer(udp_offer):
+        try:
+            udp_offer, address = udp_socket.recvfrom(1024)
+            if len(udp_offer) == UDP_OFFER_SIZE:
+                udp_offer = struct.unpack("!IBH", udp_offer)
+            else:
+                udp_offer=(0, 0, 0)
+        except socket.error:
+            udp_offer=(0, 0, 0)
+    udp_socket.close()
+    return (address, decode_port_from_offer(udp_offer))
+
+#hmmm.. tries to connect to the server?
+#returns a socket
+#or None if failed
+def try_connect_to_server(address, tcp_port):
+    tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.settimeout(1)
+    try:
+        tcp_socket.connect((address[0], tcp_port))
+        return tcp_socket
+    except socket.error:
+        print("connection timed out")
+        return None
+
+#send a string message throgh tcp socket. closes the socket if an exception was thrown
+def tcp_send(tcp_socket, msg):
+    try:
+        tcp_socket.send(str.encode(msg))
+        return True
+    except socket.error:
+        tcp_socket.close()
+        return False
+
+#send a byte message throgh tcp socket without closing the socket if an exception was thrown
+def tcp_send_byte(tcp_socket, msg):
+    try:
+        tcp_socket.send(msg)
+        return True
+    except socket.error:
+        return False
+
+#receives a message ffrom tcp socket
+#returns empty string if timed out
+#returns None if an exception was thrown
+def tcp_receive(tcp_socket):
+    try:
+        msg=tcp_socket.recvfrom(1024)[0].decode()
+        return msg
+    except socket.timeout:
+        return ""
+    except socket.error as e:
+        #print(e)
+        return None
+
+#checks if there is a pressed key
+def isData():
+        return select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
+#function that maintains the play logic
+def play(tcp_socket, old_settings):
+    tcp_socket.settimeout(0.01)
+    stop=False
+    #for a non blocking getch
+    tty.setcbreak(sys.stdin.fileno())
+    while not stop:
+        #check if some key is pressed
+        if isData():
+            pressed_key=getch.getch()[0]
+            #try sending the key
+            if not tcp_send_byte(tcp_socket, pressed_key.encode()):
+                #print("connection to the server lost")
+                tcp_socket.settimeout(2)
+                stop=True
+        #receive incoming messages
+        msg=tcp_receive(tcp_socket)
+        if msg == None:
+            #print("connection to the server lost.")
+            stop=True
+        elif msg != "":
+            print(msg)
+        #check the connection by catching an exception when sending a message to a closed (by the server) socket
+        #for some reson it refuses to send an empty string, which makes it unusable
+        #if not tcp_send(""):
+        #    print("server closed the connection.")
+        #    stop=True
+        #else:
+        #    print("s")
+    #restore the old settings of terminal
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    print("exited the server")
+    #for manual exiting from the server
+    #input()
+    tcp_socket.close()
+    #wait because the addres
+    #time.sleep(1)
+
+#function to handle the connection to the server
+def connection_handle(tcp_socket, name):
+    print("connected")
+    #try sending the name
+    if not tcp_send(tcp_socket, name+"\n"):
+        return
+    #wait for the game start
+    tcp_socket.settimeout(11)
+    game_start_msg=tcp_receive(tcp_socket)
+    if game_start_msg == None or game_start_msg == "":
+        print("the server failed to start the game")
+        return
+    print(game_start_msg)
+    #to save the old setting of terminal
+    old_settings = termios.tcgetattr(sys.stdin)
+    try:
+        play(tcp_socket, old_settings)
+    except:
+        #restore old settings of the terminal if crashed
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+#main function. duh
+def main():
+    name=startup()
+    while True:
+        address, tcp_port = look_for_servers()
+        print("Received offer from "+address[0]+", attempting to connect...")
+        tcp_socket=try_connect_to_server(address, tcp_port)
+        if tcp_socket != None:
+            connection_handle(tcp_socket, name)
+        else:
+            print("failed to connect to the server.")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        #in case of ctrl+c that is not handled
+        #for some reson it does not catch it...
+        #termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    #    pass
+        print("error:")
+        print(e)
+        print(traceback.format_exc())
+        print("closing the client")
---
+++ server.py
@@ -0,0 +1,225 @@
+import socket
+import time
+import threading
+import random
+from scapy.arch import get_if_addr
+import struct
+from colorama import Fore
+from colorama import Style
+
+UDP_PORT=13117
+TCP_PORT=2151
+
+MAGIC_NUMBER=0xfeedbeef
+MESSAGE_TYPE=0x02
+
+MAX_PLAYERS=30
+MAX_UDP_OFFERS=10
+GAME_LENGTH=10
+
+keys_buffer=([], [])
+
+grading=False
+
+COLOR_CYAN="\033[96m"
+COLOR_YELLOW="\033[93m"
+
+COLOR_END="\033[0m"
+
+def set_color(msg, color):
+    return color+msg+COLOR_END
+
+#makes and returns a udp socket
+def make_udp_socket():
+    udp_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    #udp_socket.settimeout(0.2)
+    return udp_socket
+
+#makes and returns a tcp socket
+def make_tcp_server_socket():
+    tcp_server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #for choosing a specific network
+    #cant figure out how to do it
+    #needs while so it will wait for an adress to be free
+    binded=False
+    while not binded:
+        try:
+            if grading:
+                tcp_server_socket.bind((get_if_addr("eth2"), TCP_PORT))
+            else:
+                tcp_server_socket.bind(("", TCP_PORT))
+            binded=True
+            time.sleep(0.1)
+        except:
+            pass
+    print("opened the server socket")
+    tcp_server_socket.listen(MAX_PLAYERS)
+    return tcp_server_socket
+
+#sends a message to a tcp socket
+#closes the socket if an exception was thrown and returns False
+def tcp_send(my_socket, msg):
+    try:
+        my_socket.send(str.encode(msg))
+        return True
+    except socket.error:
+        my_socket.close()
+        return False
+
+#returns a game start message
+def start_message():
+    while not finished_accepting:
+        time.sleep(0.1)
+    result=f"\n{Fore.YELLOW}Wlcome to Igor's Server!{Style.RESET_ALL}"
+    result+=f"The game has started!\n{Fore.CYAN}Team 1{Style.RESET_ALL}:\n"
+    for client_name in player_names[0]:
+        result+=client_name+"\n"
+    result+=f"--------------\n{Fore.GREEN}Team 2{Style.RESET_ALL}:\n"
+    for client_name in player_names[1]:
+        result+=client_name+"\n"
+    result+="\nType as fast as you can!"
+    return result
+
+#client thread
+def client_handler(client_socket, client_address, team_id):
+    #get the name
+    name=b""
+    try:
+        name, adress = client_socket.recvfrom(1024)
+    except socket.error:
+        print("a user lost connection")
+        client_socket.close()
+        return
+    name=name.decode()
+    name=name.replace("\n", "")
+    print(name+" has connected")
+    #add the player to the database
+    player_names[team_id].append(name)
+    #send a start message to the client
+    client_socket.settimeout(0.1)
+    if not tcp_send(client_socket, start_message()):
+        return
+    print("the game begins")
+    #while he game is on add all the keys that the client sends
+    while is_game:
+        try:
+            msg=client_socket.recvfrom(1024)[0].decode()
+            #print(msg)
+            if len(msg) != 0:
+                keys_buffer[team_id].append(msg[0])
+        except Exception:
+            pass
+        #print("1")
+    #wait for the result to be calculated
+    global game_result
+    while game_result == "":
+        time.sleep(0.01)
+    tcp_send(client_socket, game_result)
+    #time.sleep(1)
+    print("the client disconected")
+    client_socket.close()
+
+#chooses team
+def choose_team():
+    return random.randint(0, 1)
+
+#accepts all the replies and makes threads for each client
+def accept_offer_replies(tcp_server_socket):
+    global finished_accepting
+    finished_accepting=False
+    tcp_server_socket.settimeout(0.1)
+    finished=False
+    while not finished:
+        try:
+            client_socket, client_address=tcp_server_socket.accept()
+            client_thread=threading.Thread(target=client_handler, args=(client_socket, client_address, choose_team()))
+            client_thread.start()
+        except socket.error:
+            finished=True
+    finished_accepting=True
+
+#sends a usp offer
+def send_offer(udp_socket):
+    udp_socket.sendto(struct.pack("!IBH", MAGIC_NUMBER, MESSAGE_TYPE, TCP_PORT), ('<broadcast>', UDP_PORT))
+
+#sends udp offers
+def send_offers(udp_socket):
+    print("starting to send offers.")
+    for i in range(MAX_UDP_OFFERS):
+        send_offer(udp_socket)
+        #print("offer sent")
+        time.sleep(1)
+
+#what the main thread have to do while the game is on
+def game():
+    time.sleep(GAME_LENGTH)
+
+#checks who won...
+def check_who_won():
+    if len(keys_buffer[0]) > len(keys_buffer[1]):
+        return 0
+    if len(keys_buffer[0]) < len(keys_buffer[1]):
+        return 1
+    if len(keys_buffer[0]) == len(keys_buffer[1]):
+        return -1
+
+#makes the result message
+def make_result_message():
+    winning_id=check_who_won()
+    result="\n"
+    if winning_id == -1:
+        result+="It's a draw!\n"
+    else:
+        result+="The team "+str(winning_id+1)+" won!\n"
+    result+=f"{Fore.CYAN}Team 1{Style.RESET_ALL}: "+str(len(keys_buffer[0]))+" keys\n"
+    result+=f"{Fore.GREEN}Team 2{Style.RESET_ALL}: "+str(len(keys_buffer[1]))+" keys\n\n"
+    return result
+
+#logic after the game
+def post_game():
+    global game_result
+    game_result=make_result_message()
+    
+    print("the game has finished")
+    print("the keys are:")
+    print(keys_buffer)
+    print("Game over, sending out offer requests...\n")
+
+#main function
+def main():
+    udp_socket=make_udp_socket()
+    tcp_server_socket=make_tcp_server_socket()
+    global player_names
+    global keys_buffer
+    global is_game
+    while True:
+        #initialize
+        is_game=False
+        keys_buffer=([], [])
+        player_names=([], [])
+        #start broadcasting udps
+        send_offers(udp_socket)
+        #accepts clients
+        is_game=True
+        accept_offer_replies(tcp_server_socket)
+        global game_result
+        game_result=""
+        #dont start the game if there are no players
+        if len(player_names[0]) != 0 or len(player_names[1]) != 0:
+            #game
+            game()
+            post_game()
+    input()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        #in case of ctrl+c that is not handled
+        #for some reson it does not catch it...
+        #termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+        print("error:")
+        print(e)
+        print("closing the client")
+    
